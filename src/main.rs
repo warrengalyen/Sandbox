2044123fc0049600ed9d5112eb7c7dc7ef3d9749
Warren Galyen <wgalyen@gmail.com>
2020-10-04 10:16:43 -0700

    Prevent the window from becoming too small

--- src/main.rs
+++ src/main.rs
@@ -190,10 +190,18 @@ fn main() {
             },
 
             Event::MainEventsCleared => {
-                // Snap the window size to multiples of SIMULATION_SIZE when less than 20% away
                 if let Some(lr) = last_resize {
+                    // Prevent the window from becoming smaller than SIMULATION_SIZE
+                    if lr.elapsed() >= Duration::from_millis(10) {
+                        let mut surface_size = window.inner_size();
+                        surface_size.width = surface_size.width.max(SIMULATION_WIDTH as u32);
+                        surface_size.height = surface_size.height.max(SIMULATION_HEIGHT as u32);
+                    }
+                    // Snap the window size to multiples of SIMULATION_SIZE when less than 20% away
                     if lr.elapsed() >= Duration::from_millis(50) {
                         let mut surface_size = window.inner_size();
+                        surface_size.width = surface_size.width.max(SIMULATION_WIDTH as u32);
+                        surface_size.height = surface_size.height.max(SIMULATION_HEIGHT as u32);
                         let width_ratio = surface_size.width as f64 / SIMULATION_WIDTH as f64;
                         let height_ratio = surface_size.height as f64 / SIMULATION_HEIGHT as f64;
                         if (width_ratio.fract() < 0.20 || width_ratio.fract() > 0.80)
@@ -234,7 +242,7 @@ fn main() {
 
                     // Place particles
                     let n = (p1x - p2y).abs().max((p1y - p2y).abs()) as usize;
-                    for step in 0..(n + 1) {i
+                    for step in 0..(n + 1) {
                         let t = if n == 0 { 0.0 } else { step as f64 / n as f64 };
                         let x = (p1x + t * (p2x - p1x)).round() as usize;
                         let y = (p1y + t * (p2y - p1y)).round() as usize;
